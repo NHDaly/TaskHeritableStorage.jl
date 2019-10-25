f912b2268f3a09d36d934af11ada390133abbb90
Nathan Daly <nhdaly@gmail.com>
2019-10-25 14:49:43 -0400

    Make task_heritable_storage namespaced per-module.
    Most users should now access it through the macro,
    `@task_heritable_storage()[:key] = value`.

--- src/MultiTest.jl
+++ src/MultiTest.jl
@@ -11,25 +11,25 @@ import Test
     # Various helper methods for test sets
 
     function get_testset()
-        testsets = get($THS.task_heritable_storage(), :__BASETESTNEXT__, AbstractTestSet[])
+        testsets = get($THS.@task_heritable_storage(), :__BASETESTNEXT__, AbstractTestSet[])
         return isempty(testsets) ? fallback_testset : testsets[end]
     end
 
     function push_testset(ts::AbstractTestSet)
-        testsets = get($THS.task_heritable_storage(), :__BASETESTNEXT__, AbstractTestSet[])
+        testsets = get($THS.@task_heritable_storage(), :__BASETESTNEXT__, AbstractTestSet[])
         push!(testsets, ts)
-        setindex!($THS.task_heritable_storage(), testsets, :__BASETESTNEXT__)
+        setindex!($THS.@task_heritable_storage(), testsets, :__BASETESTNEXT__)
     end
 
     function pop_testset()
-        testsets = get($THS.task_heritable_storage(), :__BASETESTNEXT__, AbstractTestSet[])
+        testsets = get($THS.@task_heritable_storage(), :__BASETESTNEXT__, AbstractTestSet[])
         ret = isempty(testsets) ? fallback_testset : pop!(testsets)
-        setindex!($THS.task_heritable_storage(), testsets, :__BASETESTNEXT__)
+        setindex!($THS.@task_heritable_storage(), testsets, :__BASETESTNEXT__)
         return ret
     end
 
     function get_testset_depth()
-        testsets = get($THS.task_heritable_storage(), :__BASETESTNEXT__, AbstractTestSet[])
+        testsets = get($THS.@task_heritable_storage(), :__BASETESTNEXT__, AbstractTestSet[])
         return length(testsets)
     end
 end
--- src/TaskHeritableStorage.jl
+++ src/TaskHeritableStorage.jl
@@ -1,13 +1,36 @@
 module TaskHeritableStorage # end
 
-export task_heritable_storage
+export task_heritable_storage, @task_heritable_storage
 
-const heritable_storage_name = Symbol("##__nhdaly-task_heritable_storage__##")
-task_heritable_storage() = get!(task_local_storage(), heritable_storage_name, IdDict{Any,Any}())
-_has_task_heritable_storage() = haskey(task_local_storage(), heritable_storage_name)
+const _heritable_storage_name = Symbol("##__nhdaly-task_heritable_storage__##")
 
-function _copy_task_heritable_storage(dict)
-    task_local_storage()[heritable_storage_name] = copy(dict)
+"""
+    task_heritable_storage(m::Module)[:key] = value
+    @task_heritable_storage()[:key] = value
+"""
+function task_heritable_storage(m::Module)
+    get!(_task_heritable_storage_all_modules(), m, IdDict{Any,Any}())
+end
+_task_heritable_storage_all_modules() = get!(task_local_storage(), _heritable_storage_name, IdDict{Module,Any}())
+
+"""
+    @task_heritable_storage()[:key] = value
+This is simply a synonym for [`task_heritable_storage(@__MODULE__)`](@ref).
+"""
+macro task_heritable_storage()
+    :(task_heritable_storage($__module__))  # No esc needed, since no user inputs
+end
+
+# ----------------------
+# Replace the definition of Core._Task() to clone task_heritable_storage on construction.
+
+function _has_task_heritable_storage(m::Module)
+    haskey(task_local_storage(), _heritable_storage_name) &&
+        haskey(_task_heritable_storage_all_modules(), m)
+end
+
+function _clone_task_heritable_storage(dict)
+    task_local_storage()[_heritable_storage_name] = copy(dict)
 end
 
 # DANGEROUS: Override the Core Task() constructor to copy the task heritable storage
@@ -17,10 +40,10 @@ function __Task(@nospecialize(f), reserved_stack::Int, completion_future)
     return ccall(:jl_new_task, Ref{Task}, (Any, Any, Int), f, completion_future, reserved_stack)
 end
 function Core._Task(@nospecialize(f), reserved_stack::Int, completion_future)
-    if _has_task_heritable_storage()
-        let storage = task_heritable_storage()
+    if haskey(task_local_storage(), _heritable_storage_name)
+        let all_storage = _task_heritable_storage_all_modules()
             wrapped = () -> begin
-                _copy_task_heritable_storage(storage);
+                _clone_task_heritable_storage(all_storage);
                 f();
             end
             return __Task(wrapped, reserved_stack, completion_future)
@@ -33,13 +56,14 @@ end
 
 # -------- Convenience APIs ----------------
 """
-    task_heritable_storage(body, key, value)
+    task_heritable_storage(m::Module, key, value) do ... end
+    @task_heritable_storage(key, value) do ... end
 
 Call the function `body` with a modified task-heritable storage, in which `value` is assigned to
 `key`; the previous value of `key`, or lack thereof, is restored afterwards.
 """
-function task_heritable_storage(body::Function, key, val)
-    tls = task_heritable_storage()
+function task_heritable_storage(body::Function, m::Module, key, val)
+    tls = task_heritable_storage(m)
     hadkey = haskey(tls, key)
     old = get(tls, key, nothing)
     tls[key] = val
@@ -49,5 +73,15 @@ function task_heritable_storage(body::Function, key, val)
         hadkey ? (tls[key] = old) : delete!(tls, key)
     end
 end
+
+"""
+    @task_heritable_storage(key, value) do ... end
+
+This is simply a synonym for [`task_heritable_storage(@__MODULE__, key, val)`](@ref).
+"""
+macro task_heritable_storage(body::Function, key, val)
+    esc(:($task_heritable_storage(body, $__module__, key, val)))
+end
+
 
 end
