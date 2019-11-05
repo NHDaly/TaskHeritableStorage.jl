33965e1521349e673bca67c4bcdb02300031072d
Nathan Daly <nhdaly@gmail.com>
2019-11-04 20:56:30 -0500

    Make Tracing tests julia 1.2 compatible

--- test/Tracing.jl
+++ test/Tracing.jl
@@ -3,6 +3,12 @@ module TracingTests #end
 include("../examples/Tracing.jl")
 using .Tracing
 
+@static if VERSION >= v"1.3-"
+    import Base.Threads: @spawn
+else
+    macro spawn(e) esc(:(@async(e))) end
+end
+
 begin
     trace()
     tasks, funcs = get_traces()
@@ -13,12 +19,12 @@ end
 begin
     clear_traces()
     trace()
-    Threads.@spawn begin
+    @spawn begin
         trace()
-        Threads.@spawn begin
+        @spawn begin
             trace()
         end
-        Threads.@spawn begin
+        @spawn begin
             trace()
         end
     end
